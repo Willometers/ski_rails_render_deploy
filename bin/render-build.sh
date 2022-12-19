7adf564536d7d2cd71bbac5ce7ffb259b1b96cc8
BillyMiles <williammiles201@gmail.com>
2022-12-19 18:16:18 -0500

    seeding re-added

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:create
-# bundle exec rails db:seed
 bundle exec rails db:migrate
+bundle exec rails db:seed
