6d0f8cf6340b2a0e6121eabfe232aa0bb2faf651
BillyMiles <williammiles201@gmail.com>
2023-01-03 16:56:58 -0500

    rollback seeding

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,7 +7,8 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:create
 bundle exec rails db:migrate
-bundle exec rails db:seed
+# bundle exec rails db:seed
 
 # are tables even being made?
 # seeding not happening at right phase?
+# 
