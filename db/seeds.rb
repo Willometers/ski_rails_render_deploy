7069f987a65f0d0cfcb2804c341ab5abb992cf88
BillyMiles <williammiles201@gmail.com>
2023-01-04 15:07:09 -0500

    user seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,6 +46,8 @@ Skiarea.create(location: "12834", name: "Willard Mountain")
 Skiarea.create(location: "12496", name: "Windham Mountain")
 Skiarea.create(location: "13486", name: "Woods Valley Ski Area")
 
+User.create(email: "abc@abc.com")
+
 
 puts "done seeding"
 
