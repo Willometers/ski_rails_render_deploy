94d3938a66606876269900f14972816bc417bfdb
BillyMiles <williammiles201@gmail.com>
2023-01-04 11:54:40 -0500

    create and migrate reinstated

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:create
-# bundle exec rails db:migrate
+bundle exec rails db:create
+bundle exec rails db:migrate
 # bundle exec rails db:seed
 
 # are tables even being made?
